b6c3d633e49d7380d5a1f30ab97c02d3c5ec494d
ElHanko <71714066+ElHanko@users.noreply.github.com>
2026-09-25 16:27:51 +0200

    screen: rebuild settings page for Fre3nder

--- src/setting_panel.cpp
+++ src/setting_panel.cpp
@@ -1,4 +1,5 @@
 #include "setting_panel.h"
+
 #include "config.h"
 #include "spdlog/spdlog.h"
 #include "subprocess.hpp"
@@ -13,98 +14,181 @@ LV_IMG_DECLARE(refresh_img);
 LV_IMG_DECLARE(spoolman_img);
 LV_IMG_DECLARE(sysinfo_img);
 
-SettingPanel::SettingPanel(KWebSocketClient &c, std::mutex &l, lv_obj_t *parent, SpoolmanPanel &sm)
+namespace {
+
+constexpr uint32_t COLOR_BG = 0x080B0D;
+constexpr uint32_t COLOR_CARD = 0x11171B;
+constexpr uint32_t COLOR_CARD_PRESSED = 0x18242A;
+constexpr uint32_t COLOR_BORDER = 0x25323A;
+constexpr uint32_t COLOR_ACCENT = 0x00E5FF;
+constexpr uint32_t COLOR_TEXT = 0xFFFFFF;
+
+lv_obj_t *create_setting_card(lv_obj_t *parent,
+                              const void *image,
+                              const char *text,
+                              lv_event_cb_t callback,
+                              void *user_data)
+{
+  lv_obj_t *button = lv_btn_create(parent);
+  lv_obj_clear_flag(button, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_style_pad_all(button, 10, 0);
+  lv_obj_set_style_bg_color(button, lv_color_hex(COLOR_CARD), 0);
+  lv_obj_set_style_border_width(button, 1, 0);
+  lv_obj_set_style_border_color(button, lv_color_hex(COLOR_BORDER), 0);
+  lv_obj_set_style_radius(button, 8, 0);
+  lv_obj_set_style_shadow_width(button, 0, 0);
+  lv_obj_set_style_bg_color(button, lv_color_hex(COLOR_CARD_PRESSED), LV_STATE_PRESSED);
+  lv_obj_set_style_border_color(button, lv_color_hex(COLOR_ACCENT), LV_STATE_PRESSED);
+  lv_obj_set_style_opa(button, LV_OPA_40, LV_STATE_DISABLED);
+  lv_obj_set_flex_flow(button, LV_FLEX_FLOW_COLUMN);
+  lv_obj_set_flex_align(
+      button,
+      LV_FLEX_ALIGN_CENTER,
+      LV_FLEX_ALIGN_CENTER,
+      LV_FLEX_ALIGN_CENTER);
+  lv_obj_add_event_cb(button, callback, LV_EVENT_CLICKED, user_data);
+
+  lv_obj_t *icon = lv_img_create(button);
+  lv_img_set_src(icon, image);
+  lv_obj_set_style_img_recolor_opa(icon, LV_OPA_COVER, 0);
+  lv_obj_set_style_img_recolor(icon, lv_color_hex(COLOR_TEXT), 0);
+
+  lv_obj_t *label = lv_label_create(button);
+  lv_label_set_text(label, text);
+  lv_obj_set_width(label, LV_PCT(100));
+  lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
+  lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0);
+  lv_obj_set_style_text_font(label, &lv_font_montserrat_14, 0);
+  lv_obj_set_style_text_color(label, lv_color_hex(COLOR_TEXT), 0);
+
+  return button;
+}
+
+} // namespace
+
+SettingPanel::SettingPanel(KWebSocketClient &c,
+                           std::mutex &l,
+                           lv_obj_t *parent,
+                           SpoolmanPanel &sm)
   : ws(c)
   , cont(lv_obj_create(parent))
   , wifi_panel(l)
   , sysinfo_panel()
   , spoolman_panel(sm)
-  , wifi_btn(cont, &network_img, "WIFI", &SettingPanel::_handle_callback, this)
-  , restart_klipper_btn(cont, &refresh_img, "Restart Klipper", &SettingPanel::_handle_callback, this)
-  , restart_firmware_btn(cont, &refresh_img, "Restart\nFirmware", &SettingPanel::_handle_callback, this)
-  , sysinfo_btn(cont, &sysinfo_img, "System", &SettingPanel::_handle_callback, this)
-  , spoolman_btn(cont, &spoolman_img, "Spoolman", &SettingPanel::_handle_callback, this)
-  , guppy_restart_btn(cont, &refresh_img, "Restart Fre3nderScreen", &SettingPanel::_handle_callback, this)
+  , wifi_btn(nullptr)
+  , sysinfo_btn(nullptr)
+  , restart_klipper_btn(nullptr)
+  , restart_firmware_btn(nullptr)
+  , spoolman_btn(nullptr)
+  , screen_restart_btn(nullptr)
 {
   lv_obj_set_size(cont, LV_PCT(100), LV_PCT(100));
+  lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_set_style_pad_all(cont, 12, 0);
+  lv_obj_set_style_pad_row(cont, 10, 0);
+  lv_obj_set_style_pad_column(cont, 10, 0);
+  lv_obj_set_style_border_width(cont, 0, 0);
+  lv_obj_set_style_bg_color(cont, lv_color_hex(COLOR_BG), 0);
 
-  spoolman_btn.disable();
+  static lv_coord_t rows[] = {
+    LV_GRID_FR(1),
+    LV_GRID_FR(1),
+    LV_GRID_FR(1),
+    LV_GRID_TEMPLATE_LAST
+  };
+  static lv_coord_t cols[] = {
+    LV_GRID_FR(1),
+    LV_GRID_FR(1),
+    LV_GRID_TEMPLATE_LAST
+  };
+  lv_obj_set_grid_dsc_array(cont, cols, rows);
 
-  lv_obj_add_flag(cont, LV_OBJ_FLAG_SCROLLABLE);
-  lv_obj_set_scroll_dir(cont, LV_DIR_VER);
-  lv_obj_set_scrollbar_mode(cont, LV_SCROLLBAR_MODE_AUTO);
+  wifi_btn = create_setting_card(
+      cont, &network_img, "WiFi", &SettingPanel::_handle_callback, this);
+  sysinfo_btn = create_setting_card(
+      cont, &sysinfo_img, "System", &SettingPanel::_handle_callback, this);
+  restart_klipper_btn = create_setting_card(
+      cont, &refresh_img, "Restart\nKlipper", &SettingPanel::_handle_callback, this);
+  restart_firmware_btn = create_setting_card(
+      cont, &refresh_img, "Restart\nFirmware", &SettingPanel::_handle_callback, this);
+  spoolman_btn = create_setting_card(
+      cont, &spoolman_img, "Spoolman", &SettingPanel::_handle_callback, this);
+  screen_restart_btn = create_setting_card(
+      cont, &refresh_img, "Restart\nScreen", &SettingPanel::_handle_callback, this);
 
-  lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_COLUMN);
-  lv_obj_set_flex_align(
-      cont,
-      LV_FLEX_ALIGN_START,
-      LV_FLEX_ALIGN_CENTER,
-      LV_FLEX_ALIGN_CENTER);
-
-  lv_obj_set_style_pad_all(cont, 6, 0);
-  lv_obj_set_style_pad_row(cont, 6, 0);
-
-  lv_obj_t *buttons[] = {
-    wifi_btn.get_container(),
-    restart_klipper_btn.get_container(),
-    restart_firmware_btn.get_container(),
-    sysinfo_btn.get_container(),
-    spoolman_btn.get_container(),
-    guppy_restart_btn.get_container(),
+  struct CardPosition {
+    lv_obj_t *button;
+    uint8_t col;
+    uint8_t row;
   };
 
-  for (lv_obj_t *button : buttons) {
-    lv_obj_set_width(button, LV_PCT(100));
-    lv_obj_set_height(button, 76);
+  CardPosition cards[] = {
+    {wifi_btn, 0, 0},
+    {sysinfo_btn, 1, 0},
+    {restart_klipper_btn, 0, 1},
+    {restart_firmware_btn, 1, 1},
+    {spoolman_btn, 0, 2},
+    {screen_restart_btn, 1, 2},
+  };
+
+  for (const auto &card : cards) {
+    lv_obj_set_grid_cell(
+        card.button,
+        LV_GRID_ALIGN_STRETCH, card.col, 1,
+        LV_GRID_ALIGN_STRETCH, card.row, 1);
   }
 
-
+  lv_obj_add_state(spoolman_btn, LV_STATE_DISABLED);
 }
 
-SettingPanel::~SettingPanel() {
-  if (cont != NULL) {
-    lv_obj_del(cont);
-    cont = NULL;
-  }
+SettingPanel::~SettingPanel()
+{
+  // cont belongs to the Settings tab and is deleted with the tabview.
 }
 
-lv_obj_t *SettingPanel::get_container() {
+lv_obj_t *SettingPanel::get_container()
+{
   return cont;
 }
 
-void SettingPanel::handle_callback(lv_event_t *event) {
-  if (lv_event_get_code(event) == LV_EVENT_CLICKED) {
-    lv_obj_t *btn = lv_event_get_current_target(event);
+void SettingPanel::handle_callback(lv_event_t *event)
+{
+  if (lv_event_get_code(event) != LV_EVENT_CLICKED) {
+    return;
+  }
 
-    if (btn == wifi_btn.get_container()) {
-      spdlog::trace("wifi pressed");
-      wifi_panel.foreground();
-    } else if (btn == sysinfo_btn.get_container()) {
-      spdlog::trace("setting system info pressed");
-      sysinfo_panel.foreground();
-    } else if (btn == restart_klipper_btn.get_container()) {
-      spdlog::trace("setting restart klipper pressed");
-      ws.send_jsonrpc("printer.restart");
-    } else if (btn == restart_firmware_btn.get_container()) {
-      spdlog::trace("setting restart klipper pressed");
-      ws.send_jsonrpc("printer.firmware_restart");
-    } else if (btn == spoolman_btn.get_container()) {
-      spdlog::trace("setting spoolman pressed");
-      spoolman_panel.foreground();
-    } else if (btn == guppy_restart_btn.get_container()) {
-      spdlog::trace("restart fre3nderscreen pressed");
-      Config *conf = Config::get_instance();
-      auto init_script = conf->get<std::string>("/fre3nderscreen_init_script");
-      const fs::path script(init_script);
-      if (fs::exists(script) || init_script.rfind("service fre3nderscreen", 0) == 0) {
-        sp::call({init_script, "restart"});
-      } else {
-        spdlog::warn("Failed to restart Fre3nderScreen. Did not find restart script.");
-      }
+  lv_obj_t *button = lv_event_get_current_target(event);
+
+  if (button == wifi_btn) {
+    spdlog::trace("wifi pressed");
+    wifi_panel.foreground();
+  } else if (button == sysinfo_btn) {
+    spdlog::trace("setting system info pressed");
+    sysinfo_panel.foreground();
+  } else if (button == restart_klipper_btn) {
+    spdlog::trace("setting restart klipper pressed");
+    ws.send_jsonrpc("printer.restart");
+  } else if (button == restart_firmware_btn) {
+    spdlog::trace("setting restart firmware pressed");
+    ws.send_jsonrpc("printer.firmware_restart");
+  } else if (button == spoolman_btn) {
+    spdlog::trace("setting spoolman pressed");
+    spoolman_panel.foreground();
+  } else if (button == screen_restart_btn) {
+    spdlog::trace("restart Fre3nderScreen pressed");
+    Config *conf = Config::get_instance();
+    const auto init_script = conf->get<std::string>("/fre3nderscreen_init_script");
+    const fs::path script(init_script);
+
+    if (fs::exists(script) || init_script.rfind("service fre3nderscreen", 0) == 0) {
+      sp::call({init_script, "restart"});
+    } else {
+      spdlog::warn("Failed to restart Fre3nderScreen. Did not find restart script.");
     }
   }
 }
 
-void SettingPanel::enable_spoolman() {
-  spoolman_btn.enable();
+void SettingPanel::enable_spoolman()
+{
+  lv_obj_clear_state(spoolman_btn, LV_STATE_DISABLED);
 }
--- src/setting_panel.h
+++ src/setting_panel.h
@@ -2,10 +2,8 @@
 #define __SETTING_PANEL_H__
 
 #include "wifi_panel.h"
-
 #include "sysinfo_panel.h"
 #include "spoolman_panel.h"
-#include "button_container.h"
 #include "websocket_client.h"
 #include "lvgl/lvgl.h"
 
@@ -13,35 +11,35 @@
 
 class SettingPanel {
  public:
-  SettingPanel(KWebSocketClient &c, std::mutex &l, lv_obj_t *parent, SpoolmanPanel &sm);
+  SettingPanel(KWebSocketClient &c,
+               std::mutex &l,
+               lv_obj_t *parent,
+               SpoolmanPanel &sm);
   ~SettingPanel();
 
   lv_obj_t *get_container();
   void enable_spoolman();
-
   void handle_callback(lv_event_t *event);
 
   static void _handle_callback(lv_event_t *event) {
-    SettingPanel *panel = (SettingPanel*)event->user_data;
+    auto *panel = static_cast<SettingPanel *>(event->user_data);
     panel->handle_callback(event);
-  };
+  }
 
  private:
   KWebSocketClient &ws;
   lv_obj_t *cont;
 
   WifiPanel wifi_panel;
-
   SysInfoPanel sysinfo_panel;
   SpoolmanPanel &spoolman_panel;
-  ButtonContainer wifi_btn;
-  ButtonContainer restart_klipper_btn;
-  ButtonContainer restart_firmware_btn;
-  ButtonContainer sysinfo_btn;
-  ButtonContainer spoolman_btn;
-  ButtonContainer guppy_restart_btn;
-  
+
+  lv_obj_t *wifi_btn;
+  lv_obj_t *sysinfo_btn;
+  lv_obj_t *restart_klipper_btn;
+  lv_obj_t *restart_firmware_btn;
+  lv_obj_t *spoolman_btn;
+  lv_obj_t *screen_restart_btn;
 };
 
 #endif // __SETTING_PANEL_H__
-
